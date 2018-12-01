e3dddf39398fb061c9b6b3762ac1c1eaa7dbadb5
Matthew Bunday <zenceph@gmail.com>
2018-12-01 00:52:46 -0500

    Make it look respectable

--- src/components/image.js
+++ src/components/image.js
@@ -17,7 +17,7 @@ const Image = () => (
   <StaticQuery
     query={graphql`
       query {
-        placeholderImage: file(relativePath: { eq: "gatsby-astronaut.png" }) {
+        placeholderImage: file(relativePath: { eq: "zenchinese.png" }) {
           childImageSharp {
             fluid(maxWidth: 300) {
               ...GatsbyImageSharpFluid
@@ -26,7 +26,7 @@ const Image = () => (
         }
       }
     `}
-    render={data => <Img fluid={data.placeholderImage.childImageSharp.fluid} />}
+    render={data => <Img className="logo" fluid={data.placeholderImage.childImageSharp.fluid} />}
   />
 )
 export default Image
--- src/components/layout.js
+++ src/components/layout.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
-import Header from './header'
+import logo from '../images/zenchinese.png'
 import './layout.css'
 
 const Layout = ({ children }) => (
@@ -28,17 +28,10 @@ const Layout = ({ children }) => (
         >
           <html lang="en" />
         </Helmet>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: '0 auto',
-            maxWidth: 960,
-            padding: '0px 1.0875rem 1.45rem',
-            paddingTop: 0,
-          }}
-        >
+        <div id="logo"><a href="/"><img src={logo} alt="Logo" class="logo"/></a></div>
+        <article>
           {children}
-        </div>
+        </article>
       </>
     )}
   />
