ce62cf12a020d19da4f23154c96f149370c54395
Matt Bunday <matt.bunday@meetfabric.com>
2018-12-28 11:53:06 -0500

    try using google font loader

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,14 +48,6 @@ module.exports = {
         icon: 'src/images/zenchinese.png', // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `vollkorn`,
-        ]
-      }
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.app/offline
     'gatsby-plugin-offline',
